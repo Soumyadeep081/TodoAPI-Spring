4370b8d3da71675095b3d03a4d213a2e7ebd41a8
Soumyadeep081 <soumyadeep323@gmail.com>
2026-03-21 01:06:10 +0530

    Added Basic controller structure

---
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -0,0 +1,32 @@
+package com.example.todoapispring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+public class TodoController {
+    private static List<Todo> todoList;
+    public TodoController(){
+        todoList=new ArrayList<>();
+        todoList.add(new Todo(1,false,"Todo 1",1));
+        todoList.add(new Todo(2,true,"Todo 2",2));
+
+    }
+
+    @GetMapping("/todos")
+    public List<Todo>getTodos(){
+        return todoList;
+    }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
+
+    }
+
+}
